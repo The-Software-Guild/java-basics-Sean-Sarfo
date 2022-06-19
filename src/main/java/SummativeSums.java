6dfcd39f64bf549102919451d48abbf9bc4b4cf3
Sean-Sarfo <107259536+Sean-Sarfo@users.noreply.github.com>
2022-06-19 22:27:05 +0100

    Update SummativeSums.java

--- src/main/java/SummativeSums.java
+++ src/main/java/SummativeSums.java
@@ -1,23 +1,19 @@
 public class SummativeSums {
     public static void main(String[] args) {
 
-        // Arrays
-        int[] arrayOne = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
-        int[] arrayTwo = { 999, -60, -77, 14, 160, 301 };
-        int[] arrayThree = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99 };
+        int[] array_one = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
+        int[] array_two = { 999, -60, -77, 14, 160, 301 };
+        int[] array_three = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99 };
 
-        // Print sums of arrays
-        System.out.printf("#1 Array Sum: %d %n", calcSummativeSums(arrayOne));
-        System.out.printf("#2 Array Sum: %d %n", calcSummativeSums(arrayTwo));
-        System.out.printf("#3 Array Sum: %d %n", calcSummativeSums(arrayThree));
-    }
-
-    // Calc sum of arrays method
+        System.out.println("Num 1 Array Sum " + calcSummativeSums(array_one));
+        System.out.println("Num 2 Array Sum " + calcSummativeSums(array_two));
+        System.out.println("Num 3 Array Sum " + calcSummativeSums(array_three);
+                           
     public static int calcSummativeSums(int[] array){
-        int sum = 0;
+        int total = 0;
         for (int num : array) {
-            sum += num;
+            total += num;
         }
-        return sum;
+        return total;
     }
 }
